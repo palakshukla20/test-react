4d1dcefd2e728ff0045eeaf862a510d240689b07
palakshukla20 <palak@newput.com>
2022-12-29 00:19:31 +0530

    fixed test cases

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -55,7 +55,7 @@ class AddItem extends React.Component {
                 onChange={this.setTask}
               />
             </td>
-
+            <td>Priority:</td>
             <td>
               <input
                 id="prioity"
